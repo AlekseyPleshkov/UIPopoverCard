b6c8f432a0e4c4b0342b7b2df192e6bd4bb0e23e
AlekseyPleshkov <im@alekseypleshkov.ru>
2019-02-17 20:50:10 +0300

    Initial Commit

---
+++ UIPopoverCard/UIPopoverCard.h
@@ -0,0 +1,19 @@
+//
+//  UIPopoverCard.h
+//  UIPopoverCard
+//
+//  Created by Aleksey Pleshkov on 17/02/2019.
+//  Copyright © 2019 Aleksey Pleshkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIPopoverCard.
+FOUNDATION_EXPORT double UIPopoverCardVersionNumber;
+
+//! Project version string for UIPopoverCard.
+FOUNDATION_EXPORT const unsigned char UIPopoverCardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIPopoverCard/PublicHeader.h>
+
+
